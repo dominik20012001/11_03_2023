ce9703cfd3b6e848da68054011cb2f4bb5fd4fcd
dominik20012001 <39772120+dominik20012001@users.noreply.github.com>
2023-03-11 13:24:17 +0100

    Add files via upload
    2

---
+++ Main.java
@@ -0,0 +1,98 @@
+// import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+// public class Main { 
+//     static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.printIn("Napisz swoje imie");
+//         String imie = scanner.nextLine();
+        
+//         System.out.printIn("Siemano" + imie);
+//     }
+// }
+// ArrayList<integer>numbers = new ArrayList<integer>();
+// number.add(1);
+// number.add(2);
+// number.add(3);
+
+// Map<String,integer> agges=new HashMap<String,integer>();
+// agges.add("Janusz"35);
+// agges.add("Grazyna",30);
+// agges.add("JanuszJunior",10);
+
+// String firstName="Polski";
+// String lastName = "Janusz";
+
+
+// String fullName = firstName + "" + lastName;
+// //"Polski Janusz"
+
+// String s1 ="Hello";
+// String s2 = "world";
+// String s3 = "Hello";
+
+// String s = "Houston,we've,Had,a,Problem";
+
+// String[] words=s.split(",")
+// //[]"Houston,we've,Had,a,Problem"]
+
+// int x = 10;
+// if(x>5){
+//     System.out.printIn("x jest większe niż 5")
+// }else{
+//     System.out.printIn("x jest mniejsze lub równe 5")
+// }
+
+// int month = 2;
+// int numDays;
+// switch (month){
+//         case 2;
+//                 numDays = 28;
+//                 break;
+//         case 4;
+//         case 6;
+//         case 9;
+//         case 11;
+//             numDays = 30;
+//             break;
+    
+//     default
+//             numDays = 31;
+//             break;
+// }
+// System.out.printIn("There are" + numDays + "days in month" + month);
+
+// if (!name.isEmpty() && name.endsWith("a")) {
+//     System.out.printIn("sadzę że jesteś kobietą");
+// }else if (!name.isEmpty()) {
+//     System.out.printIn("sądzę że jesteś mężczyzną");
+// }else{
+//     System.out.printIn("znamy się?");
+// }
+// for (int i = 0; i < 5; i++) {
+//     System.out.printIn("Hell,world");
+// }
+// int i = 0;
+// while (i < 5) {
+//     System.out.printIn("Hell,world");
+//     i++;
+// }
+
+// int i =0;
+// do{
+//     System.out.printIn("Hell,world");
+//     i++;
+// } while (i < 5);
+
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        for (int i = 1 ; i < 101; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,8 @@
+int age = 25;
+double height = 1.75;
+boolean isStudent = true 
+char initial = "J";
+String name = "Janusz";
+
+int [ numbers = 1,2,3,4,5;]
+String names =("Max","Sam","Alex"); 
